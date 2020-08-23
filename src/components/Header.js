42687f6feed7be530dd0f1a913dac2a4d9ad988f
Kaio Ferreira <kaiolf11@gmail.com>
2020-08-23 19:11:53 -0300

    feat: add logout button to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { connect, useSelector } from "react-redux";
+import { connect, useSelector, useDispatch } from "react-redux";
+
+import { logout } from "../store/actions/auth";
 
 const Header = () => {
+  const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
+
+  const handleLogout = () => dispatch(logout());
 
   return (
     <Row className="bg-dark p-2">
       <Col className="d-flex justify-content-end">
         {user.name ? (
-          <h5 className="text-white m-2">Bem vindo, {user.name}</h5>
+          <>
+            <h5 className="text-white m-2">Bem vindo, {user.name}</h5>
+            <Button variant="link" onClick={handleLogout}>Sair</Button>
+          </>
         ) : (
           <>
             <Button className="mx-1">
