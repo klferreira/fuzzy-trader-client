0f23b5ff8b6099e68da2d436213c9651e25dc132
Kaio Ferreira <kaiolf11@gmail.com>
2020-08-23 18:20:26 -0300

    feat: display name in header when user logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { connect, useSelector } from "react-redux";
 
 const Header = () => {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <Row className="bg-dark p-2">
       <Col className="d-flex justify-content-end">
-        <Button className="mx-1">
-          <Link className="text-white" to="/login">Login</Link>
-        </Button>
-        <Button className="mx-1">
-          <Link className="text-white" to="/register">Cadastre-se</Link>
-        </Button>
+        {user.name ? (
+          <h5 className="text-white m-2">Bem vindo, {user.name}</h5>
+        ) : (
+          <>
+            <Button className="mx-1">
+              <Link className="text-white" to="/login">
+                Login
+              </Link>
+            </Button>
+            <Button className="mx-1">
+              <Link className="text-white" to="/register">
+                Cadastre-se
+              </Link>
+            </Button>
+          </>
+        )}
       </Col>
     </Row>
   );
 };
 
-export default Header;
+export default connect()(Header);
